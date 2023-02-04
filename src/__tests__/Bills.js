e1cf06c48a5c102c29dd8954ea5bcab168a9c6c9
Neil Bouiche <neil.bouiche@icloud.com>
2023-02-04 14:04:35 +0100

    Test des fonctions HandleClick du fichier Bills.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,9 +5,10 @@
 import { screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
+import { default as Bills } from "../containers/Bills";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import jsdom from "jsdom";
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
@@ -42,6 +43,51 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+  });
+
+  const { JSDOM } = jsdom;
+  describe("When the new bill button or the eye icon is clicked", () => {
+    let bills;
+    beforeEach(() => {
+      const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+      global.document = dom.window.document;
+      bills = new Bills({
+        document,
+        onNavigate: jest.fn(),
+      });
+    });
+
+    test("handleClickNewBill should call onNavigate with the right argument", () => {
+      const buttonNewBill = document.createElement("button");
+      buttonNewBill.setAttribute("data-testid", "btn-new-bill");
+      document.body.appendChild(buttonNewBill);
+
+      bills.handleClickNewBill();
+      expect(bills.onNavigate).toHaveBeenCalledWith(ROUTES_PATH["NewBill"]);
+    });
+
+    test("handleClickIconEye should call modal with the right argument", () => {
+      const iconEye = document.createElement("div");
+      iconEye.setAttribute("data-testid", "icon-eye");
+      iconEye.setAttribute("data-bill-url", "http://localhost/bill.jpg");
+      document.body.appendChild(iconEye);
+      global.$ = jest.fn().mockReturnValue({
+        modal: jest.fn(),
+        find: jest.fn().mockReturnValue({
+          html: jest.fn(),
+        }),
+        width: jest.fn().mockReturnValue(100),
+      });
+
+      bills.handleClickIconEye(iconEye);
+      expect($).toHaveBeenCalledWith("#modaleFile");
+      expect($().modal).toHaveBeenCalledWith("show");
+      expect($().find).toHaveBeenCalledWith(".modal-body");
+      expect($().width).toHaveBeenCalled();
+      expect($().find().html).toHaveBeenCalledWith(
+        `<div style='text-align: center;' class="bill-proof-container"><img width=50 src=http://localhost/bill.jpg alt="Bill" /></div>`
+      );
     });
   });
 });
